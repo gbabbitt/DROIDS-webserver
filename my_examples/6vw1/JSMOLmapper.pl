a1f3d83a9aa3dff50ac151afc0777b556a172f84
Gregory A Babbitt <gabsbi@rit.edu>
2022-01-20 21:08:06 -0500

    Add files via upload

--- my_examples/6vw1/JSMOLmapper.pl
+++ my_examples/6vw1/JSMOLmapper.pl
@@ -8,7 +8,9 @@
 $pdb = "6vw1_boundREDUCED.pdb";
 
 open INFILE, "<"."$pdb" or die "could not open infile";
-open OUTFILE, ">"."JSMOLmapping_$pdb.txt" or die "could not open outfile"; 
+open OUTFILE, ">"."JSMOLmapping_$pdb.txt" or die "could not open outfile";
+open OUTFILE2, ">"."KL.dat" or die "could not open outfile";
+open OUTFILE3, ">"."P.dat" or die "could not open outfile";
 open INFILE2, "<"."DROIDSfluctuationAVGchain.txt" or die "could not open infile";
 open INFILE3, "<"."adjustKStests.txt" or die "could not open infile";
 
@@ -42,16 +44,21 @@ for (my $i = 0; $i < scalar @IN2; $i++){
     $res_type = @inrow[3];
     #$chain = @inrow[4];
     $res_pos = @inrow[4];
-    if($chainID eq "A" && $res_pos == $res_pos1){print "$atom_pos\t"."$res_pos1\t"."$res_type1\t"."$KL\t"."$p_value\t"."$chainID\n";}
-    if($chainID eq "A" && $res_pos == $res_pos1){print OUTFILE "$atom_pos\t"."$res_pos1\t"."$res_type1\t"."$KL\t"."$p_value\t"."$chainID\n";}
-  
+    #$KLalt = $KL+rand(1)*$KL;
+    if($res_pos == $res_pos1){print "$atom_pos\t"."$res_pos1\t"."$res_type1\t"."$KL\t"."$p_value\t"."$chainID\n";}
+    if($res_pos == $res_pos1){print OUTFILE "$atom_pos\t"."$res_pos1\t"."$res_type1\t"."$KL\t"."$p_value\t"."$chainID\n";}
+    if($res_pos == $res_pos1){print OUTFILE2 "$KL\n";}
+    if($res_pos == $res_pos1){print OUTFILE3 "$p_value\n";}
   }
 }
-  
+ print OUTFILE2 "\n";
+ print OUTFILE3 "\n"; 
 close INFILE;
 close INFILE2;
 close INFILE3;
 close OUTFILE;
+close OUTFILE2;
+close OUTFILE3;
 
 print "END PROCESSING\n";
 exit;
